1679124b5a67019671ccaebe9a8a3b7c39011f19
thomzon <thomas.debouverie@gmail.com>
2023-02-14 10:41:24 +0100

    Initial Commit

---
+++ BerlinClockKata/BerlinClockKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockKataApp.swift
+//  BerlinClockKata
+//
+//  Created by Thomas Debouverie on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
